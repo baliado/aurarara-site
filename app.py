55cc61aa9dc646cf8b2a48a942b0f866e62d763f
baliado <jhony_x7@hotmail.com>
2026-05-27 20:55:17 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -181,4 +181,6 @@ def feedback():
     return render_template('feedback.html')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
